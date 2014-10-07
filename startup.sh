10958260045aeedda41704e00e679259d104570a
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-07 04:43:10 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,12 @@
 set -e
 
 if [ -f /etc/configured ]; then
-        mkdir -p /root/.ssh
-        ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ""
-        echo 'already configured'
+        echo "already configured"
 else
       #code that need to run only one time ....
-        
+        mkdir -p /root/.ssh
+        ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ""
+        echo "IdentityFile ~/.ssh/id_dsa" >> /etc/ssh/ssh_config
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
