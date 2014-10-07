831c2f8c4eb132a77ea920968abc849ee178febf
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-06 23:31:48 -0400

    Create tor.sh

---
+++ tor.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+### In tor.sh (make sure this file is chmod +x):
+# `/sbin/setuser xxxxx` runs the given command as the user `xxxxx`.
+# If you omit that part, the command will be run as root.
+
+exec /usr/sbin/tor >>/var/log/tor.log 2>&1
